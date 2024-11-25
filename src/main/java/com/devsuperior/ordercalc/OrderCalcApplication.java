03324865dd5d99bf0067cf5df2adc7220be316df
luizffdemoraes <lffm1994@gmail.com>
2024-11-25 07:41:00 -0300

    Projeto base

---
+++ src/main/java/com/devsuperior/ordercalc/OrderCalcApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ordercalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/ordercalc/OrderCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ordercalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
